3bb94e772c7884160cb84db7ea151fe8f8d37de2
unknown <0788746@gmail.com>
2019-08-26 00:11:16 +0300

    base functions added

--- main.go
+++ main.go
@@ -17,3 +17,30 @@ type boxTorets struct {
 func main() {
 	fmt.Println("БОКСИГЕН. версия 0.00001")
 }
+
+func genBoxy(sw, sh, sd, tm, sl, rr float32) {
+
+	w := sw - tm
+	h := sh - tm
+	d := sd - tm
+
+	bottom := genDno(w, d)
+
+}
+
+func genDno(w, h float32) boxElement {
+	//
+	theDno := new(boxElement)
+	theDno.width = w
+	theDno.height = h
+
+	return *theDno
+}
+
+func genLongSide(w, h float32) boxElement {
+
+}
+
+func genTorets(w, h, rr float32) boxTorets {
+
+}
